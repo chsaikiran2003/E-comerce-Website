a8720170344649d4d7967d8ebbcb2548929f6492
Sai Kiran <chsaikiran2003@gmail.com>
2025-03-26 11:02:32 +0530

    update

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://e-comerce-website-five.vercel.app",
+    origin: "https://e-comerce-website-seven.vercel.app",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
